1e6875742a32ac0f8de38901d88a41e2dfc7347b
craigkehl <keh19003@byui.edu>
2021-01-12 22:56:09 -0700

    jquery additions

--- web/W2/js/main.js
+++ web/W2/js/main.js
@@ -8,3 +8,18 @@ section.addEventListener('click', function(event) {
     card.style.backgroundColor = color;
   }
 });
+
+function fade() {
+  $("#card3").fadeToggle("slow");
+
+};
+
+$(document).ready(function() {
+  $("button").mouseover(function() {
+    var p = $("#card3").css("background-color", "yellow");
+    p.hide(1500).show(1500);
+    p.queue(function() {
+      p.css("background-color", "red");
+    });
+  });
+});
