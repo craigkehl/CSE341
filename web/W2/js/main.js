edfc003db671ab04020c75e285045010d8e22e05
craigkehl <keh19003@byui.edu>
2021-01-13 22:51:16 -0700

    js clean

--- web/W2/js/main.js
+++ web/W2/js/main.js
@@ -11,15 +11,5 @@ section.addEventListener('click', function(event) {
 
 function fade() {
   $("#card3").fadeToggle("slow");
+}
 
-};
-
-$(document).ready(function() {
-  $("button").mouseover(function() {
-    var p = $("#card3").css("background-color", "yellow");
-    p.hide(1500).show(1500);
-    p.queue(function() {
-      p.css("background-color", "red");
-    });
-  });
-});
