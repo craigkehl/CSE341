241d9ddf61e1ed5afe1c2356e7f70b8a47eeed37
craigkehl <keh19003@byui.edu>
2021-02-09 16:37:10 -0700

    add db connection

---
+++ web/ProjectSite/library/connections.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Proxy connection to the database
+ */ 
+
+function dbConnect() {
+
+    try
+    {
+        $dbUrl = getenv('DATABASE_URL');
+
+        $dbOpts = parse_url($dbUrl);
+
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"],'/');
+
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+
+        // this line makes PDO give us an exception when there are problems,
+        // and can be very helpful in debugging! (But you would likely want
+        // to disable it for production environments.)
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db
+    }
+    catch (PDOException $ex)
+    {
+        echo 'Error!: ' . $ex->getMessage();
+        die();
+    }
+}
