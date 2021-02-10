1b3ebe62a2892733e71e92a2a6f86fcea2f2cf08
craigkehl <keh19003@byui.edu>
2021-02-10 12:37:39 -0700

    team assignment with ajax

---
+++ web/W6/ta/js.js
@@ -0,0 +1,38 @@
+// $("#scriptureform").submit(function (event) {
+//     event.preventDefault(); //prevent default action 
+//     var post_url = $(this).attr("insert_scripture.php"); //get form action url
+//     var request_method = $(this).attr("POST"); //get form GET/POST method
+//     var form_data = new FormData(this); //Creates new FormData object
+//     $.ajax({
+//         url: post_url,
+//         type: request_method,
+//         data: form_data,
+//         contentType: false,
+//         cache: false,
+//         processData: false
+//     }).done(function (response) {
+//         getdisplayscripture();
+//         // $("#bogus").html($displaynewstring);
+//     });
+// });
+
+
+$("#scriptureform").submit(function (event) {
+  event.preventDefault();
+  console.log("before post");
+  $.ajax({
+      method: "POST",
+      url: "insert_scripture.php",
+      data: $("#scriptureform").serialize(),
+      success: function (res) {
+        console.log('success');
+        $("#response").html(res);
+        console.log(res);
+      }
+    })
+    .fail(function (message) {
+      console.log('file');
+      $("#response").text(message);
+      console.log(message);
+    });
+});
