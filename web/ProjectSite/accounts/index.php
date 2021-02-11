d1c094ce8429e02783d71cb76ecca7b906ae4c8a
craigkehl <keh19003@byui.edu>
2021-02-11 15:09:39 -0700

    remove model require

--- web/ProjectSite/accounts/index.php
+++ web/ProjectSite/accounts/index.php
@@ -3,7 +3,7 @@
 
 //Get the database and model brought into scope
 require_once '../library/connections.php';
-require_once '../model/accounts-model.php';
+// require_once '../model/accounts-model.php';
 // require_once '../library/functions.php';
 
 $db = getDb();
