b2322f7297ba2d89eee8ee4dc0e1799fc69a1858
craigkehl <keh19003@byui.edu>
2021-02-15 15:23:41 -0700

    Added a transaction to  regPerson()

--- web/ProjectSite/model/accounts-model.php
+++ web/ProjectSite/model/accounts-model.php
@@ -12,47 +12,52 @@ function regPerson(
           $parentMobile, 
           $parentEmail, 
           $hashedPassword) 
-  { 
-  // Insert Player
-  // $sql = 'INSERT INTO persons(first_name, last_name, birthdate, is_female)
-  //      VALUES (:first_name, :last_name, :birthdate, :is_female)';
-  $query = 'INSERT INTO persons(first_name, last_name, birthdate, is_female) VALUES (:first_name, :last_name, :birthdate, :is_female)';
+  {  
+    try {
+      // Insert Player
+      $db->beginTransaction();
+      
+      $query = 'INSERT INTO persons(first_name, last_name, birthdate, is_female) VALUES (:first_name, :last_name, :birthdate, :is_female)';
 
-  $stmt = $db->prepare($query);
-  $stmt->bindValue(':first_name', $playerFirstname, PDO::PARAM_STR);
-  $stmt->bindValue(':last_name', $playerLastname, PDO::PARAM_STR);
-  $stmt->bindValue(':birthdate', $playerBirthdate, PDO::PARAM_STR);
-  $stmt->bindValue(':is_female', $playerIsFemale, PDO::PARAM_BOOL);
-  // Insert the data
-  $stmt->execute();
-  // Ask how many rows changed as a result of our insert
-  $rowsChanged = $stmt->rowCount();
-  $newPlayerId = $db->lastInsertId("persons_id_seq");
-  // Return the indication of success (rows changed)
-  if ($rowsChanged) {
-    // insert player's parent
-    $query2 = 'INSERT INTO persons(first_name, last_name, mobile_number, email, password) VALUES (:first_name, :last_name, :mobile_number, :email, :password)';
+      $stmt = $db->prepare($query);
+      $stmt->bindValue(':first_name', $playerFirstname, PDO::PARAM_STR);
+      $stmt->bindValue(':last_name', $playerLastname, PDO::PARAM_STR);
+      $stmt->bindValue(':birthdate', $playerBirthdate, PDO::PARAM_STR);
+      $stmt->bindValue(':is_female', $playerIsFemale, PDO::PARAM_BOOL);
+      // Insert the data
+      $stmt->execute();
+      $rowsChanged = $stmt->rowCount();
+      $newPlayerId = $db->lastInsertId("persons_id_seq");
+      // Return the indication of success (rows changed)
+      if ($rowsChanged) {
+        // insert player's parent
+        $query2 = 'INSERT INTO persons(first_name, last_name, mobile_number, email, password) VALUES (:first_name, :last_name, :mobile_number, :email, :password)';
 
-    $stmt = $db->prepare($query2);
-    $stmt->bindValue(':first_name', $parentFirstname, PDO::PARAM_STR);
-    $stmt->bindValue(':last_name', $parentLastname, PDO::PARAM_STR);
-    $stmt->bindValue(':mobile_number', $parentMobile, PDO::PARAM_STR);
-    $stmt->bindValue(':password', $hashedPassword, PDO::PARAM_STR);
-    $stmt->bindValue(':email', $parentEmail, PDO::PARAM_STR);
-    // Insert the data
-    $stmt->execute();
-    // Ask how many rows changed as a result of our insert
-    $rowsChanged += $stmt->rowCount();
-    $newParentId = $db->lastInsertId("persons_id_seq");  
-    
-    $query3 = 'INSERT INTO parents(parent_person_id, parent_child_id) VALUES(:new_parent_id, :new_player_id)';
-    $stmt = $db->prepare($query3);
-    $stmt->bindValue(':new_parent_id', $newParentId, PDO::PARAM_INT);
-    $stmt->bindValue(':new_player_id', $newPlayerId, PDO::PARAM_INT);
-    $stmt->execute();
-    $rowsChanged += $stmt->rowCount();
-    return $rowsChanged;
-  } else {
-    return 0;
-  }
+        $stmt = $db->prepare($query2);
+        $stmt->bindValue(':first_name', $parentFirstname, PDO::PARAM_STR);
+        $stmt->bindValue(':last_name', $parentLastname, PDO::PARAM_STR);
+        $stmt->bindValue(':mobile_number', $parentMobile, PDO::PARAM_STR);
+        $stmt->bindValue(':password', $hashedPassword, PDO::PARAM_STR);
+        $stmt->bindValue(':email', $parentEmail, PDO::PARAM_STR);
+        // Insert the data
+        $stmt->execute();
+        // Ask how many rows changed as a result of our insert
+        $rowsChanged += $stmt->rowCount();
+        $newParentId = $db->lastInsertId("persons_id_seq");  
+      }
+
+      if ($rowsChanged > 1) {
+        $query3 = 'INSERT INTO parents(parent_person_id, parent_child_id) VALUES(:new_parent_id, :new_player_id)';
+        $stmt = $db->prepare($query3);
+        $stmt->bindValue(':new_parent_id', $newParentId, PDO::PARAM_INT);
+        $stmt->bindValue(':new_player_id', $newPlayerId, PDO::PARAM_INT);
+        $stmt->execute();
+        $rowsChanged += $stmt->rowCount();
+        $db->commit();
+        return $rowsChanged;
+      }
+    } catch (\Throwable $e) {
+        $db->rollback();
+        throw $e;
+    }  
 };
