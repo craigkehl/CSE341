7a225fb4833139e3193c1d8245e513db4c3705f6
craigkehl <keh19003@byui.edu>
2021-02-15 10:15:00 -0700

    uncomment model path in accounts controller

--- web/ProjectSite/view/includes/commonScripts.php
+++ web/ProjectSite/view/includes/commonScripts.php
@@ -1,3 +1,11 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="/ProjectSite/js/main.js"></script> 
+
+<!-- src="ProjectSite/js/main"
+
+heroku.com/
+herokyu.com/ProjectSite/js/main 
+
+heoku.com/accounts
+heoku.com/accounts/ProjectSite/js/main  -->
